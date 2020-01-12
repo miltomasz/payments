77cd845f1781d1b079067a17c830d88ed65c2e41
Tomasz Milczarek <tomasz.milczarek@gmail.com>
2020-01-12 19:35:05 +0100

    Initial Commit

---
+++ Payments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Payments
+//
+//  Created by Tomasz Milczarek on 12/01/2020.
+//  Copyright © 2020 Plumya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
